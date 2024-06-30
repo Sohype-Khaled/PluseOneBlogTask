5f704f7a9456e3542ec4c57f6c889d46d7e43a29
Sohype-Khaled <sohype.mop@gmail.com>
2024-06-30 15:21:38 +0300

    update settings

--- blog/settings.py
+++ blog/settings.py
@@ -16,12 +16,12 @@ from pathlib import Path
 
 import environ
 
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
